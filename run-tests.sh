6100cdfa17cbcf52778ae893e145e39d3c8beec3
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2026-04-09 12:15:42 +0200

    chore: run test script

---
+++ run-tests.sh
@@ -0,0 +1,32 @@
+dir=$(pwd)
+js=$(find "$dir" -maxdepth 1 -type f -name "package.json")
+java=$(find "$dir" -maxdepth 1 -type f -name "gradlew.*")
+
+if [ "$js" ]; then
+    echo "Environnement Javascrit détecté."
+    echo "Création du dossier 'test-results/'"
+
+    mkdir -p test-results
+
+    npm test -- --watch=false
+    testResult=$?
+
+    cp -r ./reports ./test-results
+
+    exit $testResult
+fi
+
+if [ "$java" ]; then
+    echo "Environnement Java détecté."
+    echo "Création du dossier 'test-results/'"
+
+    mkdir -p test-results
+
+    ./gradlew clean test
+    testResult=$?
+
+    cp -r ./build/reports ./test-results
+    exit $testResult
+fi
+
+exit 1
